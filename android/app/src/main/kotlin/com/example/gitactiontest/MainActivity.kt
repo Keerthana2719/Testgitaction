e3e8c6008c97fbf176a7f096156ca91293055955
DELL <babutn1977@gmail.com>
2025-11-16 15:16:11 +0530

    First

---
+++ android/app/src/main/kotlin/com/example/gitactiontest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gitactiontest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Samplegitaction extends StatefulWidget {
+  const Samplegitaction({super.key});
+
+  @override
+  State<Samplegitaction> createState() => _SamplegitactionState();
+}
+
+class _SamplegitactionState extends State<Samplegitaction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("First push to git")
+        ],
+      ),
+    );
+  }
+}
